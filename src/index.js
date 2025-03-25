9a44e741d27be6a9593b272b6d02ac34dff134fa
Michael <michael.onyango2@student.moringaschool.com>
2025-03-25 23:18:32 +0300

    The final final version(I hope)

--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
-// Your code here
+fetch("http://localhost:3000/characters")
+    .then(response => response.json())
+    .then(data => {
+        characters = data
+        nameEach()
+    })
+const characterNames = document.getElementById("character-bar")
+characterNames.innerHTML=""
+const votes = document.getElementById("vote-count");
+function displayCharacter(character){
+    const image = document.querySelector("#detailed-info img")
+    image.src=`${character.image}`;
+    image.alt=`${character.name}`;
+    const name = document.querySelector("#detailed-info p");
+    name.innerHTML = character.name;
+    votes.innerHTML = character.votes;
+}
+function nameEach(){
+    characters.forEach(character => {
+        const characterName = document.createElement("span")
+        characterName.innerHTML = character.name
+        characterName.addEventListener("click", () => {
+            displayCharacter(character)
+        })
+        characterNames.appendChild(characterName)
+    });
+}
+const votesForm = document.getElementById("votes-form");
+votesForm.addEventListener("submit", (event) => {
+    event.preventDefault(); 
+    const inputVotes = document.getElementById("votes");
+    const additional = parseInt(inputVotes.value, 10);
+    if (!isNaN(additional)) {
+        const currentVotes = parseInt(votes.innerHTML, 10);
+        const updatedVotes = currentVotes + additional;
+        votes.innerHTML = updatedVotes;
+        inputVotes.value = "";
+    }
+});
